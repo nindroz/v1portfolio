3a200c0f578e0e04868effaea771f53124622e7e
hitesh <mantha.hitesh@gmail.com>
2020-06-20 22:18:58 -0700

    made dark mode look a lot better with a new icon and new colors

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+window.addEventListener("load", () =>{
+    if(localStorage.getItem("theme")=="dark"){
+        document.querySelector("body").setAttribute("class","dark")
+        localStorage.setItem("theme","dark")
+        const card = document.querySelectorAll(".card");
+        card.forEach(item =>{
+            item.setAttribute("class"," card text-overflow text-white bg-dark border-light");
+        });
+    }
+    else{
+        document.querySelector("body").setAttribute("class","light")
+        localStorage.setItem("theme","light")
+        const card = document.querySelectorAll(".card");
+        card.forEach(item =>{
+            item.setAttribute("class"," card text-overflow");
+        });
+    }
+})
 function changedark(){
-    if(document.querySelector("body").className=="light"){
+    if(localStorage.getItem("theme")=="light"){
     document.querySelector("body").setAttribute("class","dark")
-    
+    localStorage.setItem("theme","dark")
     const card = document.querySelectorAll(".card");
     card.forEach(item =>{
         item.setAttribute("class"," card text-overflow text-white bg-dark border-light");
@@ -9,7 +27,7 @@ function changedark(){
 }
 else{
     document.querySelector("body").setAttribute("class","light")
-    
+    localStorage.setItem("theme","light")
     const card = document.querySelectorAll(".card");
     card.forEach(item =>{
         item.setAttribute("class"," card text-overflow");
